e889479e3c224da5f0b3e590323df560a8b22421
dvir <dvir019@gmail.com>
2019-09-04 23:59:11 +0300

    Add documentation to 1_3 and 1_4

--- Mission_1/1_3.c
+++ Mission_1/1_3.c
@@ -1,4 +1,4 @@
-// Mission 1, exercise 3
+// 1_3.c
 
 #include <stdio.h>
 
@@ -9,14 +9,36 @@
 #define TIME_ANSWER "\nThere are %d years, %d months and %d weeks in this time period.\n"
 #define NUMBER_PLACEHOLDER "%d"
 
+//-----------------------------------------------------------------------------
+//                               Convert Days
+//                               ------------
+//
+// General : Converts amount of days into years, months and weeks.
+//
+// Input   : The number of days.
+//
+// Process : Calculate the number of years, months and weeks by
+//			 divion and modulo.
+//           * Assumption: There are 30 days in a month.
+//
+// Output  : Print the number of years, months and weeks.
+//
+//-----------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 
+// Date       : 04.09.2019
+//-----------------------------------------------------------------------------
 void main(void)
 {
+	// Variables Defenition
 	unsigned short usTotalDays;
 
+	// Get the number of days
 	printf(NUMBER_OF_DAYS_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &usTotalDays);
 
+	// Print the answers
 	printf(TIME_ANSWER, usTotalDays / DAYS_IN_YEAR,
-		  (usTotalDays%DAYS_IN_YEAR) / DAYS_IN_MONTH,
-		  ((usTotalDays % DAYS_IN_YEAR) % DAYS_IN_MONTH) / DAYS_IN_WEEK);
+		   (usTotalDays % DAYS_IN_YEAR) / DAYS_IN_MONTH,
+		   ((usTotalDays % DAYS_IN_YEAR) % DAYS_IN_MONTH) / DAYS_IN_WEEK);
 }
--- Mission_1/1_4.c
+++ Mission_1/1_4.c
@@ -1,30 +1,51 @@
-// Mission 1, exercise 4
+// 1_4.c
 
 #include <stdio.h>
 
 #define DAYS_OF_WATERING (365 / 3)
 #define GARDEN_AREA_REQUEST "Enter the area of the garden: "
 #define CUBIC_METER_PRICE_REQUEST "Enter the price of cubic meter of water: "
-#define WATER_CONSUMPTION_REQUEST "Enter your water consumption: "
+#define WATER_CONSUMPTION_REQUEST "Enter the water consumption per cubic meter: "
 #define TOTAL_PRICE_ANSWER "\nWatering your garden costs %f dollars per year.\n"
 #define NUMBER_PLACEHOLDER "%f"
 
+//-----------------------------------------------------------------------------
+//                              Garden Watering
+//                              ---------------
+//
+// General : Calculates the price of watering a garden.
+//
+// Input   : The area of the garden, the price of one cubic meter of water,
+//			 and the water consumption per cubic meter.
+//
+// Process : Multuply the three numbers and the number of watering days.
+//
+// Output  : Print the total price of one year of watering the garden.
+//
+//-----------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 
+// Date       : 04.09.2019
+//-----------------------------------------------------------------------------
 void main(void)
 {
+	// Variables Defenition
 	float fGardenArea;
 	float fCubicMeterPrice;
 	float fWaterConsumption;
 
+	// Get the number of area of the garden
 	printf(GARDEN_AREA_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &fGardenArea);
 
+	// Get the price of cubic meter of water
 	printf(CUBIC_METER_PRICE_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &fCubicMeterPrice);
 
+	// Get the water consumption per cubic meter
 	printf(WATER_CONSUMPTION_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &fWaterConsumption);
 
+	// Print the answer
 	printf(TOTAL_PRICE_ANSWER, fGardenArea * fCubicMeterPrice * fWaterConsumption * DAYS_OF_WATERING);
-
-	int x; scanf("%d", &x);
 }

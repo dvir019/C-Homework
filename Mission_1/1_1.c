edc6192b97f6ede3e55c613b81cd0aead5c62b8b
dvir <dvir019@gmail.com>
2019-09-04 23:29:30 +0300

    Add documentation to 1_1 and 1_2

--- Mission_1/1_1.c
+++ Mission_1/1_1.c
@@ -1,4 +1,4 @@
-// Mission 1, exercise 1
+// 1_1.c
 
 #include <stdio.h>
 
@@ -7,47 +7,29 @@
 #define MINUTS_IN_HOUR 60
 
 //-----------------------------------------------------------------------------
-//                             
-//                             -----------------
+//                         Clerk Work Time And Salary
+//                         --------------------------
 //
-// General : 
+// General : Calculates the work time and the salary of a clerk.
 //
-// Input   : 
+// Input   : None.
 //
-// Process :
+// Process : Calculates the total time of the work in minutes,
+//           and uses it to calculate the hours and minutes.
 //
-// Output  : 
+// Output  : None.
 //
 //-----------------------------------------------------------------------------
-// Programmer :
-// Student No :
-// Date       :
+// Programmer : Dvir Twito
+// Student No : 
+// Date       : 04.09.2019
 //-----------------------------------------------------------------------------
 void main(void)
 {
+	// Variables Defenition
 	unsigned short num_of_forms = 25;
 	unsigned short usTotalWorkTime = num_of_forms * FORM_TIME_IN_MINUTES;
 	unsigned short usWorkTimeInHours = usTotalWorkTime / MINUTS_IN_HOUR;
 	unsigned short usWorkTimeInMinutes = usTotalWorkTime % MINUTS_IN_HOUR;
 	unsigned short usSalary = num_of_forms * FORM_SALARY_IN_SHEKELS;
-	printf("aaaa");
 }
-
-
-//-----------------------------------------------------------------------------
-//                             
-//                             -----------------
-//
-// General : 
-//
-// Input   : 
-//
-// Process :
-//
-// Output  : 
-//
-//-----------------------------------------------------------------------------
-// Programmer :
-// Student No :
-// Date       :
-//-----------------------------------------------------------------------------
--- Mission_1/1_2.c
+++ Mission_1/1_2.c
@@ -1,4 +1,4 @@
-// Mission 1, exercise 2
+// 1_2.c
 
 #include <stdio.h>
 
@@ -7,17 +7,37 @@
 #define NUMBER_OF_TAXIES_AND_PASSENGERS_ANSWER "\nIt will require %d taxies, and %d will stay in the station.\n"
 #define NUMBER_PLACEHOLDER "%d"
 
+//-----------------------------------------------------------------------------
+//                             Taxies Passengers
+//                             -----------------
+//
+// General : Calculates the number of taxies and the remaining passengers.
+//
+// Input   : The number of passengers and the number of seats in a taxi.
+//
+// Process : Calculate the quotient and the remainder of the numbers.
+//
+// Output  : Print the quotient and the remainder.
+//
+//-----------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 
+// Date       : 04.09.2019
+//-----------------------------------------------------------------------------
 void main(void)
 {
 	// Variables Defenition
 	unsigned short usNumberOfPassengers;
 	unsigned short usNumberOfSeats;
 
+	// Get the number of passangers
 	printf(NUMBER_OF_PASSENGERS_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &usNumberOfPassengers);
 
+	// Get the number of seats in a taxi
 	printf(NUMBER_OF_SEATS_REQUEST);
 	scanf(NUMBER_PLACEHOLDER, &usNumberOfSeats);
 
+	// Print the answers
 	printf(NUMBER_OF_TAXIES_AND_PASSENGERS_ANSWER, usNumberOfPassengers / usNumberOfSeats, usNumberOfPassengers % usNumberOfSeats);
 }

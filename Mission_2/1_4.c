405a00d19a78120e3a78196c2491d9d3f9a0bc00
dvir <dvir019@gmail.com>
2019-09-07 20:10:31 +0300

    Start 1_2 and 1_4

---
+++ Mission_2/1_4.c
@@ -0,0 +1,12 @@
+// 1_4.c
+
+#define M 10.5
+#define T 5.5
+
+void main(void)
+{
+	// Variables defenition
+	float bananasWeight = M + 2;
+	float bananasPricePerKg = 2 * T;
+	float totalPrice = M * T + bananasWeight * bananasPricePerKg;
+}

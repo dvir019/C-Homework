6004ad94a68f7c2a2a63f2e95e4d3f6db049047c
dvir <dvir019@gmail.com>
2019-09-16 20:03:03 +0300

    Ass documentation for 4_1_4

--- Mission_4/Mission_4_1/4_1_4.c
+++ Mission_4/Mission_4_1/4_1_4.c
@@ -6,6 +6,23 @@
 #define TWO 2
 #define THREE 3
 
+//---------------------------------------------------------------------------------
+//                                 Date To Season
+//                                 --------------
+//
+// General : Convert a number represents a date into a season.
+//
+// Input   : An eight-digit number that represents a date in the format DDMMYYY.
+//
+// Process : Determine the season by the result of dividing thr month by three.
+//
+// Output  : The season of the date.
+//
+//---------------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 324270883
+// Date       : 15.09.2019
+//---------------------------------------------------------------------------------
 void main(void)
 {
 	// Variables defenition
@@ -14,9 +31,11 @@ void main(void)
 	unsigned short year;
 	unsigned short monthDivideByThree;
 
+	// Get the number, and store its parts
 	printf("Enter a date, in format DDMMYYYY:\n");
 	scanf("%02hu%02hu%04hu", &day, &month, &year);
 
+	// Find the season according to the month and print it.
 	monthDivideByThree = month / THREE;
 
 	switch (monthDivideByThree)

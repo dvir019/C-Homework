95130fafcc22df3e52bb716db774379b1238c54c
dvir <dvir019@gmail.com>
2019-09-16 18:44:16 +0300

    Add documentation for 4_1_3

--- Mission_4/Mission_4_1/4_1_3.c
+++ Mission_4/Mission_4_1/4_1_3.c
@@ -1,6 +1,25 @@
 // 4_1_3.c
+
 #include <stdio.h>
 
+//---------------------------------------------------------------------------------
+//                                 Number To Date
+//                                 ---------------
+//
+// General : Gets a number, and turns it into a date.
+//
+// Input   : An eight-digit number that represents a date in the format DDMMYYY.
+//
+// Process : Get the number and store its parts in variables for the day, the month
+//			 and the year.
+//
+// Output  : A message representing that date.
+//
+//---------------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 324270883
+// Date       : 15.09.2019
+//---------------------------------------------------------------------------------
 void main(void)
 {
 	// Variables defenition
@@ -8,8 +27,10 @@ void main(void)
 	unsigned short month;
 	unsigned short year;
 
+	// Get the number
 	printf("Enter a date, in format DDMMYYYY:\n");
 	scanf("%02hu%02hu%04hu", &day, &month, &year);
 
+	// Print the message
 	printf("\nThe year is %04hu, the month is %02hu, and the day is %02hu", year, month, day);
 }

cca3b91cedc63ef4acc2eb75fd35fad29d49e01c
dvir <dvir019@gmail.com>
2019-09-18 19:23:10 +0300

    Add 4_2_3

---
+++ Mission_4/Mission_4_2/4_2_3.c
@@ -0,0 +1,11 @@
+/*
+if (bishopX - kingX == bishopY - kingY || bishopX - kingX == kingY - bishopY)
+{
+	// Threatened
+}
+
+if (ABS(bishopX - kingX) == ABS(bishopY - kingY))
+{
+	// Threatened
+}
+*/

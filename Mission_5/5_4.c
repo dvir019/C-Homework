9f582f835c511d8c61ba330d55d0414660d9b81e
dvir <dvir019@gmail.com>
2019-09-23 15:39:58 +0300

    Finish 5_4

--- Mission_5/5_4.c
+++ Mission_5/5_4.c
@@ -1,27 +1,51 @@
 // 5_4.c
 
 #include <stdio.h>
+#include "../Utils/Macros.c"
 
 #define TEN 10
+#define ZERO 0
 
+//---------------------------------------------------------------------------------
+//                               Biggest Two Numbers
+//                               -------------------
+//
+// General : Finds the biggest two numbers from ten elements.
+//
+// Input   : Ten numbers.
+//
+// Process : Compare every new number with the two biggest numbers, and change
+//			 them if needed.
+//
+// Output  : The two biggest numbers.
+//
+//---------------------------------------------------------------------------------
+// Programmer : Dvir Twito
+// Student No : 324270883
+// Date       : 23.09.2019
+//---------------------------------------------------------------------------------
 void main(void)
 {
-	float max;
-	float secondMax;
-	float number;
-	int counter = 0;
+	// Variables defenition
+	unsigned short number;
+	unsigned short max = ZERO;
+	unsigned short secondMax = ZERO;
+	int counter = TEN;
 
-	for (counter = TEN; counter; counter--)
+	// Get the numbers and find the biggest two
+	for (; counter; counter--)
 	{
+		// Get a number
 		printf("Enter a number: ");
-		scanf("%f", &number);
+		scanf("%hu", &number);
 
-		if (number > max)
-		{
-			secondMax = max;
-			max = number;
-		}
+		// Compare the number withthe currently two biggest numbers.
+		secondMax = ((number > max) ?
+					(max) :
+					((number == max) ? (secondMax) : (MAX(number, secondMax))));
+		max = MAX(number, max);
 	}
 
-	printf("The biggest numbers are %f and %f", max, secondMax);
+	// Print the result
+	printf("The biggest numbers are %hu and %hu", max, secondMax);
 }

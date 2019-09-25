072118c5b338fabe565fba43f95bc6282d6c5c22
dvir <dvir019@gmail.com>
2019-09-25 17:18:42 +0300

    Finish 6_2

--- Mission_6/6_2.c
+++ Mission_6/6_2.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 #include "../Utils/Functions.c"
 
+//---------------------------------------------------------------------------------
+//                                 Sum Of New Numbers
+//                                 ------------------
+//
+// General      : Calculates the sum of the number that created from the even
+//				  digits of the number and the number that created from the odd
+//				  digits of the number.
+//
+// Parameters   :
+//      number - The number.
+//
+// Return Value : The sum of the number that created from the even digits of the
+//				  number and the number that created from the odd digits of the
+//				  number.
+//
+//---------------------------------------------------------------------------------
 int SumOfNewNumbers(int number)
 {
 	// Variables defenition
@@ -17,4 +33,9 @@ int SumOfNewNumbers(int number)
 
 	// Return the sum
 	return (evenDigits + oddDigits);
+}
+
+void main(void)
+{
+	printf("The sum is: %d", SumOfNewNumbers(5243617));
 }
